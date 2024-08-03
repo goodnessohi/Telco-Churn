bf738d31ee7051c9936acb45806bdcf32d37fa36
goodnessohi <76630943+goodnessohi@users.noreply.github.com>
2024-08-03 22:03:43 +0100

    Update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -54,6 +54,7 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
+    
 
     data_transformation = DataTransformation()
     train_arr, test_arr, *additional_values = data_transformation.initiate_data_transformation(train_data, test_data)
